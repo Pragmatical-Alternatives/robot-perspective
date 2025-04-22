fb895af4232823cbd2a73ad7055ea5624cc50048
ietzel <67884277+ietzel@users.noreply.github.com>
2025-04-22 17:05:15 -0400

    Update main.rb

--- main.rb
+++ main.rb
@@ -1,1 +1,1 @@
-puts "Robots need degrees of freedom to make step-by-step decisions."
+puts "Robots need degrees of freedom to make step-by-step decisions. As reality is more often made of objects than subjects, it is easy for robots to gain understanding."
